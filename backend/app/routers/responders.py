4c9641944db513527ba54c6a98ec82b844854d67
Randy Treit <randyt@outlook.com>
2025-08-19 19:54:23 -0700

    Revert "Merge pull request #39 from scvsar/feature/simplify"
    This reverts commit fdfd2e5b210cc106cbf9b6e3f9fcfc819e66ef5c, reversing
    changes made to 28e82ec8325c369a0cc7ded9adda6a545311bbd8.

--- backend/app/routers/responders.py
+++
@@ -1,9 +0,0 @@
-from typing import List
-from fastapi import APIRouter
-
-router = APIRouter()
-
-@router.get("/api/responders")
-async def get_responders() -> List[dict]:
-    import main  # type: ignore
-    return main.messages
